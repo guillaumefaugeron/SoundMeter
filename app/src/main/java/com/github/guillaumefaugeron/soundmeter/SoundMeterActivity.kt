c947721416231951d946974f18b143dcb94cd0f5
Guillaumefaugreon <guillaume.faugeron@ynov.com>
2020-12-08 12:23:16 +0100

    add background runnig

--- app/src/main/java/com/github/guillaumefaugeron/soundmeter/SoundMeterActivity.kt
+++ app/src/main/java/com/github/guillaumefaugeron/soundmeter/SoundMeterActivity.kt
@@ -1,26 +1,22 @@
 package com.github.guillaumefaugeron.soundmeter
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.pm.PackageManager
-import android.location.Criteria
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.location.Location
-import android.location.LocationListener
 import android.location.LocationManager
-import android.os.*
-import android.util.Log
-import android.util.Log.DEBUG
-import android.util.Log.INFO
-import android.widget.Switch
+import android.os.Bundle
+import android.os.Vibrator
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.github.guillaumefaugeron.soundmeter.BuildConfig.DEBUG
 import com.google.android.material.slider.Slider
-import java.util.*
-
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
 class SoundMeterActivity: AppCompatActivity() {
 
     companion object {
@@ -32,58 +28,60 @@ class SoundMeterActivity: AppCompatActivity() {
     }
 
     private var popupAlreadyDisplayed = false
-    private var locationManager: LocationManager? = null
+    private var speed: Float = 50.0f
 
+    private lateinit var startStopBtn: Button
     private lateinit var latitudeView: TextView
     private lateinit var longitudeView: TextView
-    private var speed: Float = 50.0f
+    private lateinit var speedView: TextView
+
 
     //    private lateinit var speedView: TextView
     private lateinit var slider: Slider
 
 
-    private var locationListener = LocationListener { location ->
-
+    val locationConsumer = Consumer<Location> { location ->
         latitudeView.text = String.format("Latitude %.6f",location.latitude)
         longitudeView.text = String.format("Longitude %.6f",location.longitude)
-        if (location.hasSpeed()){
-            slider.addOnChangeListener { slider, value, fromUser ->
-                     speed = value
-            }
 
-            if(location.speed > speed){
-            val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-            if (vibrator.hasVibrator()) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
-                    Toast.makeText(applicationContext, "You are too fast", Toast.LENGTH_SHORT).show()
-                } else {
-                    vibrator.vibrate(500)
-                    Toast.makeText(applicationContext, "You are too fast", Toast.LENGTH_SHORT).show()
-                }
-            }
-            }
+        speedView.text = String.format("Speed : %.6f",location.speed, " m/s")
+
+        slider.addOnChangeListener { slider, value, fromUser ->
+            speed = value
+            System.out.println(value)
         }
 
 
-
+        if (location.speed > speed) {
+            // threshold reached
+            (getSystemService(VIBRATOR_SERVICE) as Vibrator).apply {
+                vibrate(500)
+            }
+            Toast.makeText(this, "too fast", Toast.LENGTH_SHORT).show()
+        }
     }
+
+    var locationDisposable: Disposable? = null
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sound_meter)
-        /*
-        setContentView(LinearLayout(this).apply {
-            orientation = LinearLayout.VERTICAL
-            addView(ImageView(this@SoundMeterActivity).apply {
-                setImageResource(R.mipmap.ic_launcher)
-            })
-        })*/
         popupAlreadyDisplayed = savedInstanceState?.getBoolean(POPUP_DISPLAYED) ?: false
         latitudeView = findViewById(R.id.latitude)
         longitudeView = findViewById(R.id.longitude)
+        speedView = findViewById(R.id.speed)
         slider = findViewById(R.id.speed2)
         speed = slider.value
+        startStopBtn = findViewById(R.id.start)
+        startStopBtn.setOnClickListener {
+            LocationService.startOrStop(this)
+            syncUI() // TODO
+        }
+        popupAlreadyDisplayed = savedInstanceState?.getBoolean(POPUP_DISPLAYED) ?: false
+        speedView = findViewById(R.id.speed)
     }
 
     override fun onStart() {
@@ -92,37 +90,61 @@ class SoundMeterActivity: AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED) {
             if (supportFragmentManager.findFragmentByTag(TAG_PERMISSION_FRAGMENT) == null
-                && !popupAlreadyDisplayed) {
+                    && !popupAlreadyDisplayed) {
                 supportFragmentManager
-                    .beginTransaction()
-                    .add(PermissionPopupFragment(), TAG_PERMISSION_FRAGMENT)
-                    .commit()
+                        .beginTransaction()
+                        .add(PermissionPopupFragment(), TAG_PERMISSION_FRAGMENT)
+                        .commit()
                 popupAlreadyDisplayed = true
             }
-        } else {
-            locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
-            locationManager?.requestLocationUpdates(
-                LOCATION_UPDATE_MS,
-                LOCATION_UPDATE_RADIUS,
-                Criteria().apply {
-                    accuracy = Criteria.ACCURACY_FINE
-                }, locationListener, Looper.myLooper())
-
-
         }
+        syncUI()
+        locationDisposable = LocationService.locationObservable.subscribe(locationConsumer)
+
     }
 
     override fun onPause() {
         super.onPause()
-        locationManager?.removeUpdates(locationListener)
+        locationDisposable?.dispose()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putBoolean(POPUP_DISPLAYED, popupAlreadyDisplayed)
+    }
+
+
+
+//    @SuppressLint("MissingPermission")
+//    private fun startOrStop() {
+//        if (locationManager == null) {
+//            locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+//            locationManager?.requestLocationUpdates(
+//                    LOCATION_UPDATE_MS,
+//                    LOCATION_UPDATE_RADIUS,
+//                    Criteria().apply {
+//                        accuracy = Criteria.ACCURACY_FINE
+//                    }, locationListener, Looper.myLooper()
+//            )
+//        } else {
+//            locationManager?.removeUpdates(locationListener)
+//            locationManager = null
+//            speedView.text = String.format("Speed : tracking stopped")
+//
+//        }
+//        syncUI()
+//    }
+
+    private fun syncUI() {
+        if (LocationService.isRunning) {
+            startStopBtn.text = getString(R.string.btn_stop)
+        } else {
+            startStopBtn.text = getString(R.string.btn_start)
+        }
+        startStopBtn.isEnabled =
+                ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED
     }
 
 }
